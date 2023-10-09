de7f2b6f1fe4fb9533365b0293c7a525104f12da
adyson <adyv8@yahoo.com.br>
2023-10-09 16:27:36 -0300

    5° commit - tdd model: flight_number preenchido? GREEN

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
   
+  before {
+    @flight = Flight.new
+    }
+  
   describe "Teste de preenchimento dos campos do model Flight" do
     
     it "flight_number preenchido?" do
       @flight.flight_number = "1234"
-      expect(@flight.flight_number).to ed("1234")
+      expect(@flight.flight_number).to eq("1234")
     end
     
   end
