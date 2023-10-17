0eaae410cb8dfdb70b2a747d2f0bf14130d2a08c
adyson <adyv8@yahoo.com.br>
2023-10-17 16:28:47 -0300

    24° commit - tdd controller: Consegue listar todos os voos e retornar status 200? RED

--- spec/controllers/v1/flights_controller_v1_spec.rb
+++ spec/controllers/v1/flights_controller_v1_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Api::V1::FlightsController, type: :controller do
     it 'Consegue listar todos os voos e retornar status 200' do
       get :index
       expect(JSON.parse(response.body).size).to eq(1)
-      expect(response).to have_http_status(200)
+      #expect(response).to have_http_status(200)
     end
     
   end
