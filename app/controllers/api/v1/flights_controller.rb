1fcbd992a1067cf448fd3a3973b96af84e2fd101
adyson <adyv8@yahoo.com.br>
2023-10-17 16:46:50 -0300

    25° commit - tdd controller: Consegue listar todos os voos e retornar status 200? GREEN

--- app/controllers/api/v1/flights_controller.rb
+++ app/controllers/api/v1/flights_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::FlightsController < ApplicationController
   
+  before_action :set_flight, only: %i[show update destroy]
+  
   def index
-    
+    @flights = Flight.all
+    render json: @flights
+  end
+
+private
+
+  def set_flight
+    @flight = Flight.find(params[:id])
+  end
+  
+  def flight_params
+    params.require(:flight).permit(:flight_number, :flight_company, :flight_from, :flight_to, :flight_gate, :flight_hour, :flight_status)
   end
   
 end
--- spec/controllers/v1/flights_controller_v1_spec.rb
+++ spec/controllers/v1/flights_controller_v1_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::FlightsController, type: :controller do
   
+  before{
+    @flight = Flight.create(flight_number: '1234', flight_company: 'latam', flight_from: 'bel', flight_to: 'poa', flight_gate: '2', flight_hour: '18:45', flight_status: 'confirmed')
+    }
+  
   describe 'GET /api/v1/flights' do
     
-    it 'Consegue listar todos os voos e retornar status 200' do
+    it 'Consegue listar todos os voos e retornar status 200?' do
       get :index
       expect(JSON.parse(response.body).size).to eq(1)
-      #expect(response).to have_http_status(200)
+      expect(response).to have_http_status(200)
     end
     
   end
