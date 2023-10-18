f70fd64d44460e4fa1de340a3225f61029d1783a
adyson <adyv8@yahoo.com.br>
2023-10-17 21:42:29 -0300

    26° commit - tdd controller: Consegue listar todos os voos e retornar status 200? REFACTOR

--- spec/controllers/v1/flights_controller_v1_spec.rb
+++ spec/controllers/v1/flights_controller_v1_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Api::V1::FlightsController, type: :controller do
   
   before{
-    @flight = Flight.create(flight_number: '1234', flight_company: 'latam', flight_from: 'bel', flight_to: 'poa', flight_gate: '2', flight_hour: '18:45', flight_status: 'confirmed')
-    }
+    @flight = Flight.create(flight_number: "1234", flight_company: "latam", flight_from: "bel", flight_to: "poa", flight_gate: "2", flight_hour: "18:45", flight_status: "confirmed")
+  }
   
   describe 'GET /api/v1/flights' do
     
@@ -13,7 +13,7 @@ RSpec.describe Api::V1::FlightsController, type: :controller do
       expect(JSON.parse(response.body).size).to eq(1)
       expect(response).to have_http_status(200)
     end
-    
+        
   end
   
 end
